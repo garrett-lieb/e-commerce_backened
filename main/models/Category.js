d1b4e08be932cb45ae9e80b822a269ba00dc7423
garrett lieb <liebg08@gmail.com>
2024-02-07 12:19:57 -0800

    updated model files

--- main/models/Category.js
+++ main/models/Category.js
@@ -7,11 +7,23 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+    // allows for camelcase in column names
     underscored: true,
     modelName: 'category',
   }
--- main/models/Product.js
+++ main/models/Product.js
@@ -10,6 +10,33 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 10.00
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      },
+    },
   },
   {
     sequelize,
